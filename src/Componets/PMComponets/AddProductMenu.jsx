5bec963b412650f7b6aab82d9e668c4ac5f745e0
chigoox <dikeemmanyel54@gmail.com>
2023-02-08 19:28:50 -0500

    profuct page done jus need to finish editing items

--- src/Componets/PMComponets/AddProductMenu.jsx
+++ src/Componets/PMComponets/AddProductMenu.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import { handleInput5, addProductInfoToDatabase, uploadfile, deleteFile } from '../../MyCodes/ed5'
+import { handleInput5, addProductInfoToDatabase, uploadfile, deleteFile,  } from '../../MyCodes/ed5'
 import Uploading from '../Uploading'
 
 
 function AddProductMenu({ toggleShowAdd }) {
     const [productData, setProductData] = useState({category:'Female'})
     const [showUpload, setShowUpload] = useState(0)
-    
- function addToShop(productData){
-    addProductInfoToDatabase(productData, productData.name)
-    toggleShowAdd()
- }
+   
+    function addToShop(productData){
+        addProductInfoToDatabase(productData, productData?.name)
+        toggleShowAdd()
+    }
 
- const openUpload = () => {
-    document.getElementById('upload').click();
-  }
+    const openUpload = () => {
+        document.getElementById('upload').click();
+    }
 
   return (
-    <div className='fixed w-screen h-screen flex bg-slate-900 bg-opacity-95 transition-all'>
+    <div className='fixed z-10 w-screen h-screen flex bg-slate-900 bg-opacity-95 transition-all'>
        {showUpload && <Uploading per={showUpload} setUpload={setShowUpload}/>}
-        <div className="relative shadow-md shadow-black bg-slate-400 rounded-3xl h-[32rem] w-[90%] m-auto lg:w-[20%]">
+        <div className="relative shadow-md shadow-black bg-slate-400 rounded-3xl h-[33rem] w-[90%] m-auto lg:w-[20%]">
             <div className='flex'>
                 <div className='h-16 w-16 bg-slate-900  rounded-full m-3'>
                     <button className='h-full w-full rounded-full' onClick={openUpload}></button>
@@ -28,7 +28,7 @@ function AddProductMenu({ toggleShowAdd }) {
                 <button onClick={()=>{deleteFile('img');toggleShowAdd()}} className="self-center right-2 top-2 ml-12 h-12 w-20 bg-slate-900 text-rose-900 text-2xl font-bold rounded-full">X</button>
             </div>
             <div className=' m-auto w-[90%] h-[70%] relative'>
-                <input  name={'img'} id='upload' type='file' onChange={({target})=>{uploadfile(target.name, setProductData, setShowUpload )}} accept="image/png, image/jpeg" hidden/>
+                <input  name={`${productData.name}`} id='upload' type='file' onChange={({target})=>{uploadfile(target.name, setProductData, setShowUpload )}} accept="image/png, image/jpeg" hidden/>
                 <input onChange={(event)=>{handleInput5(event,setProductData)}} className='w-full h-14 rounded-full my-2 text-2xl font-bold p-3 shadow-sm shadow-black' placeholder='Product Name' type="text" name="name" id="" />
                 <input onChange={(event)=>{handleInput5(event,setProductData)}} className='w-full h-14 rounded-full my-2 text-2xl font-bold p-3 shadow-sm shadow-black' placeholder='$Product Price' type="number" name="price" id="" />
                 <textarea onChange={(event)=>{handleInput5(event,setProductData)}} className='w-full h-24 my-2 rounded-3xl font-bold p-4 shadow-sm shadow-black' placeholder='Product Description' type="text" name="desc" id="" />
@@ -40,7 +40,8 @@ function AddProductMenu({ toggleShowAdd }) {
                         <option value="Boy">Boy</option>
                         </optgroup>
                 </select>
-               <button className='h-14 w-[80%] mx-8 my-4 bg-blue-600 text-white font-bold text-3xl rounded-full' onClick={()=>{addToShop(productData)}}>Add to Shop</button>
+               <button className='h-14 w-[80%] mx-8 my-4 bg-blue-600 text-white font-bold text-3xl rounded-full' onClick={()=>{if(productData.name) addToShop(productData)}}>Add to Shop</button>
+                <h1 className='relative bottom-4 text-center font-bold text-xl text-rose-700'>name required!</h1>
             </div>
        
        
--- src/Pages/ProductManager/ManageProductPage.jsx
+++ src/Pages/ProductManager/ManageProductPage.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddProductMenu from '../../Componets/PMComponets/AddProductMenu'
+import { fetchProducts, deleteProduct } from '../../MyCodes/ed5'
+
+
 function ManageProductPage() {
   const [showAdd, setShowAdd] = useState(false)
   const [showRemove, setShowRemove] = useState(false)
+  const [ProductsFIREBASE, setProductsFIREBASE] = useState({})
+  const products = Object.values(ProductsFIREBASE)
+
   const toggleShowAdd = () => {console.log('working');setShowAdd(!showAdd)}
   const toggleShowRemove = () => {setShowRemove(!showRemove)}
+  useEffect(()=>{fetchProducts(setProductsFIREBASE)},[showAdd,showRemove])
   
-  function ProductIcon(){
+
+  function ProductIcon({ img, name, price }){
+    
     return(
-      <div className=' h-36 w-24 relative'>
-        {showRemove && <button className='absolute right-0 -top-2 w-8 h-8 rounded-full bg-slate-900 text-rose-800 font-bold'>X</button>}
-        <div className='m-auto rounded-2xl w-20 h-20 bg-white'>
-        
+      <div className=' h-36 w-24 lg:w-32 relative m-auto'>
+        {showRemove && <button onClick={()=>{deleteProduct(name)}} className='absolute right-0 -top-2 w-8 h-8 rounded-full bg-slate-900 text-rose-800 font-bold'>X</button>}
+        <div className='m-auto rounded-2xl w-20 h-20 lg:w-32 lg:h-32 bg-white'>
+          
+        {img && <img className='z-30 h-full w-full' src={img} alt="noIMG" />}
         </div>
-        <h1 className='font-bold text-white text-center text-lg'>extra wig glue</h1>
+       <div className='lg:w-32 '>
+          <h1 className=' font-bold text-white text-center text-lg'>{name}</h1>
+          <h1 className='border-rose-700 border shadow-black shadow font-bold text-white text-center'>${price}</h1>
+       </div>
       </div>
     )
   }
   
-  
+  const productMAP = products.map(({name, price, desc, img})=>{
+    return(<ProductIcon key={name} name={name} price={price} desc={desc} img={img}/>)
+  })
   
   
   
@@ -32,9 +47,9 @@ function ManageProductPage() {
           <button onClick={toggleShowAdd} className=' bg-lime-400 rounded-full h-24 w-40 m-5 font-bold text-white border-4 border-emerald-400'>Add</button>
           <button onClick={toggleShowRemove} className='  bg-rose-400 rounded-full h-24 w-40 m-5 font-bold text-white border-4 border-orange-300'>{`${!showRemove ? 'Remove':'Cancel'}`}</button>
         </div>
-        <div className="p-5 py-10 grid gap-10 grid-cols-3 grid-flow-row-dense bg-slate-800 h-[35rem] rounded-t-[5rem] mt-5 overflow-y-scroll">
+        <div className="p-5 lg:p-20 py-10 grid gap-10 lg:gap-16 lg:grid-cols-4 grid-cols-3 grid-flow-row-dense bg-slate-800 h-[35rem] rounded-t-[5rem] mt-5 overflow-y-scroll">
 
-        {<ProductIcon />}
+        {productMAP}
 
 
         </div>
